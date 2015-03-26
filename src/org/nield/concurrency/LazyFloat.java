a4a417f9ce2c4ec449e243bac3eb7ef04e93d60b
TOM-SURFACE\Thomas <thomasnield@live.com>
2015-03-26 13:08:07 -0500

    implement LazyFloat

---
+++ src/org/nield/concurrency/LazyFloat.java
@@ -0,0 +1,41 @@
+package org.nield.concurrency;
+
+/**<html>A simple but powerful concurrency utility that simplifies lazy initialization. <br><br>Client simply provides a
+ * supplier for the lazy-initialized <b>float</b> value to the static factory method <b><i>forSupplier()</i></b><br><br>
+ * A LazyObject will be returned by the static factory, and the first time <b>getAsFloat()</b> is called, the <b>float</b> value is initialized and cached in a concurrent/threadsafe manner.
+ * </html>
+ */
+public final class LazyFloat {
+    private final FloatSupplier floatSupplier;
+    private volatile boolean updated = false;
+    private volatile float value;
+
+    private LazyFloat(FloatSupplier floatSupplier) {
+        this.floatSupplier = floatSupplier;
+    }
+    public float getAsFloat() {
+        if (! updated) {
+            synchronized(this) {
+                if (! updated) {
+                    value = floatSupplier.getAsFloat();
+                    updated = true;
+                }
+            }
+        }
+        return value;
+    }
+    public void reset() {
+        if (updated) {
+            synchronized(this) {
+                if (updated) {
+                    updated = false;
+                }
+            }
+        }
+    }
+
+    @FunctionalInterface
+    public interface FloatSupplier {
+        public float getAsFloat();
+    }
+}
