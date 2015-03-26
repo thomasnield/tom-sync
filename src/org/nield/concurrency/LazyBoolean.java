c729682b8fdbbaee186ced0c81eede7fdfbc266f
TOM-SURFACE\Thomas <thomasnield@live.com>
2015-03-26 12:43:53 -0500

    edit documentation for LazyBoolean

--- src/org/nield/concurrency/LazyBoolean.java
+++ src/org/nield/concurrency/LazyBoolean.java
@@ -3,8 +3,8 @@ package org.nield.concurrency;
 import java.util.function.BooleanSupplier;
 
 /**<html>A simple but powerful concurrency utility that simplifies lazy initialization. <br><br>Client simply provides a
- * supplier for the lazy-initialized <b>int</b> value to the static factory method <b><i>forSupplier()</i></b><br><br>
- * A LazyObject will be returned by the static factory, and the first time <b>get()</b> is called, the <b>int</b> value is initialized and cached in a concurrent/threadsafe manner.
+ * supplier for the lazy-initialized <b>boolean</b> value to the static factory method <b><i>forSupplier()</i></b><br><br>
+ * A LazyObject will be returned by the static factory, and the first time <b>get()</b> is called, the <b>boolean</b> value is initialized and cached in a concurrent/threadsafe manner.
  * </html>
  */
 public final class LazyBoolean {
